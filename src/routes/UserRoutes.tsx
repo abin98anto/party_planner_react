269e5b1cc8f312d1c371885972d6ffa87c7fef4f
abin98anto <abin98anto@gmail.com>
2025-04-12 11:14:08 +0530

    dummy home page created

--- src/routes/UserRoutes.tsx
+++ src/routes/UserRoutes.tsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import UserLayout from "../components/user/UserLayout/UserLayout";
+
 const UserRoutes = () => {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route element={<UserLayout />}>
+          <Route path="/" />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default UserRoutes;
