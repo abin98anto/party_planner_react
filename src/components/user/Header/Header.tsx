cbe4e9e0ed19309322aacc1c39557f4aec99962c
abin98anto <abin98anto@gmail.com>
2025-04-12 11:10:22 +0530

    modified the header.

--- src/components/user/Header/Header.tsx
+++ src/components/user/Header/Header.tsx
@@ -1,51 +1,25 @@
 import type React from "react";
 import { Link } from "react-router-dom";
-import { Avatar, Menu, MenuItem, IconButton } from "@mui/material";
 
 import "./Header.scss";
+import { Button, Stack } from "@mui/material";
 
 const Header: React.FC = () => {
   return (
     <header className="user-header">
       <div className="user-container">
         <Link to="/" className="user-logo">
-          SkillForge
+          Party Planner
         </Link>
 
-        <nav className="user-nav">
-          <ul>
-            <li>
-              <Link to="#">Courses</Link>
-            </li>
-            <li>
-              <Link to="#">My Learning</Link>
-            </li>
-            <li>
-              <Link to="#">Subscriptions</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <div>
-          <IconButton>
-            <Avatar
-              sx={{
-                width: 40,
-                height: 40,
-                border: "2px solid #primary.main",
-              }}
-            >
-              U
-            </Avatar>
-          </IconButton>
-          <Menu anchorEl={null} open={false}>
-            <MenuItem>
-              <Link to="#" style={{ textDecoration: "none", color: "inherit" }}>
-                Profile
-              </Link>
-            </MenuItem>
-            <MenuItem>Logout</MenuItem>
-          </Menu>
+        <div className="user-header-links">
+          <div>
+            <Stack spacing={2} direction="row">
+              <Button variant="text">Home</Button>
+              <Button variant="text">Products</Button>
+              <Button variant="outlined">Login</Button>
+            </Stack>
+          </div>
         </div>
       </div>
     </header>
