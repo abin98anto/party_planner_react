c4bdea97a2744a7b3b0457d48e2175c7943bf60c
abin98anto <abin98anto@gmail.com>
2025-04-12 09:59:44 +0530

    added styles to the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import UserLayout from "./components/user/UserLayout/UserLayout";
+import NotFound from "./components/common/NotFound/NotFound";
 
 function App() {
-  return <div>the app</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/*" element={<UserLayout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
