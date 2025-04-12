cb62dca3722d8b58d8b559251df632527ae1c6f3
abin98anto <abin98anto@gmail.com>
2025-04-12 11:25:14 +0530

    added footer to layout.

--- src/components/user/UserLayout/UserLayout.tsx
+++ src/components/user/UserLayout/UserLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import "./UserLayout.scss";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footert";
 
 const UserLayout = () => {
   return (
@@ -9,6 +10,7 @@ const UserLayout = () => {
       <main className="user-content" style={{ flex: "1" }}>
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 };
